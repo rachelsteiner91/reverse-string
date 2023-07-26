f84febfc4951c3f90f457a367c0cd450a0dcb754
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-26 09:26:51 -0600

    reversed string

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,4 +1,10 @@
 function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i > -1; --i) {
+    reversed = reversed + str[i]
+  }
+  return reversed;
   // type your code here
 }
 
